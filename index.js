ac1ca27342e159a5e7fd96219ecf29eca8216d96
Frank Rousseau <frank.rousseau@cozycloud.cc>
2016-02-12 19:12:17 +0100

    add start function

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-console.log('ok');
 var express = require('express')
 var serveStatic = require('serve-static')
 
 var app = express()
 
-port = process.env.PORT || 3000;
-app.use(serveStatic('app/'))
-app.listen(port, function () {
-  console.log('Server is listening on port: ' + port);
-});
 
+function start (callback) {
+  port = process.env.PORT || 3000;
+  app.use(serveStatic('app/'))
+  app.listen(port, function () {
+    console.log('Server is listening on port: ' + port);
+  });
+  if (callback !== undefined)
+    callback();
+}
+
+module.exports = start;
+
+if (!module.parent) { start(); }
